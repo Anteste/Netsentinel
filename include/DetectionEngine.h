55729c2eb1ac22172be3fda4ef312435b59d4c9e
Iliass Alami-Qammouri <iliass.alami-qammouri@cassini.de>
2026-06-12 13:51:13 +0200

    feat: implement detection engine

--- include/DetectionEngine.h
+++ include/DetectionEngine.h
@@ -1,8 +1,33 @@
 #ifndef IDS_DETECTION_ENGINE_H
 #define IDS_DETECTION_ENGINE_H
 
-class DetectionEnginePlaceholder
+#include "Alert.h"
+#include "NetworkEvent.h"
+
+#include <memory>
+#include <optional>
+#include <vector>
+
+class DetectionRule
 {
+public:
+    virtual ~DetectionRule() = default;
+    virtual std::optional<Alert> inspect(const NetworkEvent& event, int nextAlertId) = 0;
+};
+
+class DetectionEngine
+{
+public:
+    void addRule(std::unique_ptr<DetectionRule> rule);
+    std::vector<Alert> processEvent(const NetworkEvent& event);
+    int processedEventCount() const;
+    int generatedAlertCount() const;
+
+private:
+    std::vector<std::unique_ptr<DetectionRule>> rules;
+    int processedEvents{0};
+    int generatedAlerts{0};
+    int nextAlertId{1};
 };
 
 #endif
--- src/DetectionEngine.cpp
+++ src/DetectionEngine.cpp
@@ -1,1 +1,35 @@
 #include "DetectionEngine.h"
+
+void DetectionEngine::addRule(std::unique_ptr<DetectionRule> rule)
+{
+    rules.push_back(std::move(rule));
+}
+
+std::vector<Alert> DetectionEngine::processEvent(const NetworkEvent& event)
+{
+    ++processedEvents;
+    std::vector<Alert> alerts;
+
+    for (auto& rule : rules)
+    {
+        auto alert = rule->inspect(event, nextAlertId);
+        if (alert)
+        {
+            alerts.push_back(*alert);
+            ++nextAlertId;
+            ++generatedAlerts;
+        }
+    }
+
+    return alerts;
+}
+
+int DetectionEngine::processedEventCount() const
+{
+    return processedEvents;
+}
+
+int DetectionEngine::generatedAlertCount() const
+{
+    return generatedAlerts;
+}
